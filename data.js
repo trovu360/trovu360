dc5b3051fc25484b1a6970ddef02a264f10e5ed3
trovu360 <38672542+trovu360@users.noreply.github.com>
2018-04-23 20:02:32 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,56 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-faxada",
+      "name": "Faxada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.07009088962631793,
+        "pitch": -0.1279836014207305,
+        "fov": 1.290060274579411
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10597274930946377,
+          "pitch": 0.01179407170368485,
+          "rotation": 0,
+          "target": "0-faxada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0168787179726522,
+          "pitch": 0,
+          "title": "Entrada subsolo",
+          "text": "entrada elegante"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
